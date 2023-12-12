5e248e9f4e3c706e8bacde698e7f4e95f0158964
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-12-12 16:20:15 +0700

    Added Preloader component for loading the page.

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 "use client";
 import styles from "./page.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Project from "../components/Project";
 import Landing from "../components/Landing";
 import Modal from "../components/Modal";
 import Description from "../components/Description";
 import SlidingImages from "../components/SlidingImages";
 import Contact from "../components/Contact";
+import { AnimatePresence } from "framer-motion";
+import Preloader from "../components/Preloader";
 
 const projects = [
   {
@@ -32,10 +34,31 @@ const projects = [
 ];
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+
+      const locomotiveScroll = new LocomotiveScroll();
+    })();
+
+    setTimeout(() => {
+      setIsLoading(false);
+
+      document.body.style.cursor = "default";
+
+      window.scrollTo(0, 0);
+    }, 2000);
+  }, []);
+
   const [modal, setModal] = useState({ active: false, index: 0 });
 
   return (
     <>
+      <AnimatePresence mode="wait">
+        {isLoading && <Preloader />}
+      </AnimatePresence>
       <Landing />
       <Description />
       <main className={styles.main}>
