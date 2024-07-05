e34dd8907c99a37b75c4df3c246377e19ff5c0aa
LynnT-2003 <u6411271@au.edu>
2024-07-06 04:11:40 +0700

    Added links for projects.

--- src/app/page.js
+++ src/app/page.js
@@ -9,16 +9,19 @@ import SlidingImages from "../components/SlidingImages";
 import Contact from "../components/Contact";
 import { AnimatePresence } from "framer-motion";
 import Preloader from "../components/Preloader";
+import { useRouter } from "next/navigation";
 
 const projects = [
   {
     title: "TBH-Chatbot Web App",
+    linl: "https://tbh-chatbot-product-page.vercel.app/",
     subtitle: "Sophomore Year Senior Project",
     src: "lucy.png",
     color: "#000000",
   },
   {
     title: "TutorPlus Mobile App",
+    link: "https://tutorplus-landing-page-amwqk50o7-lynnt-2003.vercel.app/",
     subtitle: "University D*Code Club Project",
     src: "police.jpg",
     color: "#EFE8D3",
@@ -31,13 +34,16 @@ const projects = [
   },
   {
     title: "Fullstack E-Commerce ",
+    link: "https://github.com/LynnT-2003/Lynnime-Animelist",
     subtitle: "Junior Year Summer Web Project",
+    link: "https://ecommerce-sanity-stripe-lynnt-2003.vercel.app/",
     src: "rebec.jpg",
     color: "#8C8C8C",
   },
 ];
 
 export default function Home() {
+  const router = useRouter();
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -75,13 +81,21 @@ export default function Home() {
         <div className={styles.body}>
           {projects.map((project, index) => {
             return (
-              <Project
-                index={index}
-                title={project.title}
-                subtitle={project.subtitle}
-                setModal={setModal}
-                key={index}
-              />
+              <div
+                onClick={() => {
+                  if (project.link) {
+                    router.push(project.link);
+                  }
+                }}
+              >
+                <Project
+                  index={index}
+                  title={project.title}
+                  subtitle={project.subtitle}
+                  setModal={setModal}
+                  key={index}
+                />
+              </div>
             );
           })}
         </div>
